867ca833572e6e3881129051535362e71a2cf90a
Chandan Kumar VS <64135289+ChandanKumarVS@users.noreply.github.com>
2020-06-08 12:37:16 +0530

    Add files via upload

---
+++ repeating elements .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int i,arr[20],j,n;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+    printf("Enter any %d elements in array: ",n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Duplicate elements are: ");
+    for(i=0; i<n; i++)
+    {   for(j=i+1; j<n; j++)
+        {   if(arr[i]==arr[j])
+            {   printf("%d\n",arr[i]);
+            }
+        }
+    }
+
+}
