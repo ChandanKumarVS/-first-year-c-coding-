72164c857a3dd6adae9d9def907a00b75bfc4f12
Chandan Kumar VS <64135289+ChandanKumarVS@users.noreply.github.com>
2020-06-11 16:14:08 +0530

    Add files via upload

---
+++ sum and difference of matrix .c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i,j;
+    int first[10][10],second[10][10],sum[10][10],dif[10][10];
+
+    printf("\nEnter the number of rows and columns of the matrix");
+    scanf("%d %d",&m,&n);
+
+    printf("\nEnter the elements of the first matrix");
+    for (i = 0; i< m; i++)
+        for (j = 0; j< n; j++)
+            scanf("%d",&first[i][j]);
+
+    printf("\nEnter the elements of the second matrix");
+
+    for (i = 0; i < m; i++)
+        for (j= 0; j< n; j++)
+            scanf("%d",&second[i][j]);
+
+    for (i= 0; i < m; i++)
+        for (j= 0; j< n; j++)
+        {
+            sum[i][j] = first[i][j] + second[i][j];
+            dif[i][j] = first[i][j] - second[i][j];
+        }
+
+    printf("\nThe sum of the two matrices is : \n\n");
+    for (i= 0; i < m; i++)
+        for (j= 0; j< n; j++)
+        {
+            printf(" %d \n",sum[i][j]);
+        }
+    printf("\nThe diff of the two matrices is : \n\n");
+    for (i= 0; i < m; i++)
+        for (j= 0; j< n; j++)
+        {
+            printf(" %d \n",dif[i][j]);
+        }
+
+    getchar();
+    return 0;
+}
