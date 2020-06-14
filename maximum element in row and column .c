5e9d5f302e1099bd5a03200ba67693e9b9742d4b
Chandan Kumar VS <64135289+ChandanKumarVS@users.noreply.github.com>
2020-06-14 13:27:59 +0530

    Add files via upload

---
+++ maximum element in row and column .c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define size 3
+int main()
+{
+    int a[size][size];
+    int r, c, max = 0;
+    printf("Enter elements in matrix of size %dx%d: \n", size, size);
+    for(r=0; r<size; r++)
+    {   for(c=0; c<size; c++)
+        {   scanf("%d", &a[r][c]);
+        }
+    }
+    for(r=0; r<size; r++)
+    {   max = 0;
+        for(c=0; c<size; c++)
+        {   max= a[r][c];
+        }
+        printf(" max elements of Row %d = %d\n", r+1, max);
+    }
+    for(r=0; r<size; r++)
+    {
+        max = 0;
+        for(c=0; c<size; c++)
+        {   max = a[c][r];
+        }
+        printf("max  elements of Column %d = %d\n", r+1,max);
+    }
+    return 0;
+}
